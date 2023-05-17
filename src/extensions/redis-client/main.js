828649b3a946f17617544c6d4a224fc8fefa811e
Oybek Maxsudov <programmingiswonderful@gmail.com>
2023-05-17 16:24:28 +0500

    redis connection updated

--- src/extensions/redis-client/main.js
+++ src/extensions/redis-client/main.js
@@ -6,7 +6,6 @@ module.exports = {
     return new Redis({
       port: +getValueOrUseDefault('REDIS_PORT', 5432), // Redis port
       host: getValueOrUseDefault('REDIS_HOST', '127.0.0.1'), // Redis host
-      username: getValueOrUseDefault('REDIS_USERNAME', 'default'), // needs Redis >= 6
       password: getValueOrUseDefault('REDIS_PASSWORD', 'my-top-secret'),
       db: +getValueOrUseDefault('REDIS_DB', 0), // Defaults to 0
     });
